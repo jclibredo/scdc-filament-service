bf50ee48d82d67b6977dc259dfb19b212dd4d8ac
jclibredo <jclibredo@gmail.com>
2026-07-25 12:05:45 +0800

    YML CI/CD Code

--- cron.sh
+++ cron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/u164809439/domains/scdc-web-app.com/laravel-app
-/usr/bin/php artisan schedule:run >> /dev/null 2>&1
+/usr/bin/php artisan queue:work --stop-when-empty --tries=3 >> storage/logs/cron.log 2>&1
